b822c7c21267372909fe27690baa351ef95ae807
Paul Welby <pwelby@gmail.com>
2023-07-13 07:32:27 -0700

    update production envirnment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   API_URL: 'https://json-server-data-onrender.onrender.com/',
   MONGO_URL: 'https://angular15-signup-verify-backend.onrender.com'
 };
