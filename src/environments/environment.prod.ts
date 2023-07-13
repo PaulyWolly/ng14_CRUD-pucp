7bd5d8c28fdb1d61067330790b5ca0c59f4344e5
Paul Welby <pwelby@gmail.com>
2023-07-13 07:28:45 -0700

    update envirnments files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  API_URL: 'https://json-server-pucp.herokuapp.com/',
-  // API_URL: 'https://json-server-pucp.cyclic.app/',
+  production: false,
+  API_URL: 'https://json-server-data-onrender.onrender.com/',
+  MONGO_URL: 'https://angular15-signup-verify-backend.onrender.com'
 };
