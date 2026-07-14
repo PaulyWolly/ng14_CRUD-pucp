432ed5da4191ef98390eb67f8776746f65cc331f
Paul Welby <c205428@ardentmills.com>
2026-07-14 13:37:10 -0700

    Point production API_URL at the new Render json-server service.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://json-server-data-onrender.onrender.com/',
+  API_URL: 'https://ng14-crud-pucp.onrender.com/',
   MONGO_URL: 'https://angular15-signup-verify-backend.onrender.com'
 };
